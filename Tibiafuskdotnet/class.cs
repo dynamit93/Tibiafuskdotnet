a6e3aec29273ccb9cffcfe36010fddfb6abd5aa7
dennis lindström <dell_t_@hotmail.com>
2021-09-09 22:01:19 +0200

    GetProcessesByName i en class

---
+++ Tibiafuskdotnet/class.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tibiafuskdotnet
+{
+    class Class1
+    {
+
+        static void Main(string[] args)
+        {
+
+            appRunning("Tibia");
+
+        }
+
+        static void appRunning(string appName = "optinal")
+        {
+            Process[] localByName = Process.GetProcessesByName("Tibia");
+            //Process[] ProcessList = Process.GetProcesses();
+
+            foreach(Process p in localByName)
+            {
+                Console.WriteLine(p.ProcessName);
+                if(p.ProcessName.Contains(appName))
+                {
+                    ///lägg in program
+                    
+                }
+                /*else
+                {
+                    Console.WriteLine("start tibia");
+                }*/
+                
+            }
+            Console.ReadLine();
+        }
+
+
+    }
+}
