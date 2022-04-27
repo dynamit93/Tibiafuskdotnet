40f17b349d1caeaaf91971ce43ec7e6135610374
dennis lindström <dell_t_@hotmail.com>
2022-04-27 18:44:44 +0200

    upgrade targetscriptedit but compile error

--- Tibiafuskdotnet/MVVM/ViewModel/TargetMenuViewModel.cs
+++ Tibiafuskdotnet/MVVM/ViewModel/TargetMenuViewModel.cs
@@ -216,21 +216,7 @@ namespace Tibiafuskdotnet.ViewModel
 
 
 
-        public void loadtargeting()
-        {
 
-            DirectoryInfo d = new DirectoryInfo(@"D:\Test"); //Assuming Test is your Folder
-            FileInfo[] Files = d.GetFiles("*.txt"); //Getting Text files
-            string str = "";
-
-            foreach (FileInfo file in Files)
-            {
-                str = str + ", " + file.Name;
-            }
-            
-
-
-        }
 
 
         public RelayCommand<string> command { get; set; }
